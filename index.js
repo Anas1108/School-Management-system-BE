b035f3c85f677edd57b0c9ab8dc7d7de005a51c6
anas zafar <anaszafar1108@gmail.com>
2026-02-10 16:04:18 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+// const bodyParser = require("body-parser")
+const app = express()
+const Routes = require("./routes/route.js")
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000
+
+console.log("Starting server...");
+console.log("MONGO_URL:", process.env.MONGO_URL);
+
+// app.use(bodyParser.json({ limit: '10mb', extended: true }))
+// app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
+
+app.use(express.json({ limit: '10mb' }))
+app.use(cors())
+
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+
+app.use('/', Routes);
+
+app.get('/', (req, res) => {
+    res.send("Hello to School Management System");
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server started at port no. ${PORT}`)
+})
